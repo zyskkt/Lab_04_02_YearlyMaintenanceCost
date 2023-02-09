7c72f393833536c862ca8e16525284af0ee46636
zyskkt <122432583+zyskkt@users.noreply.github.com>
2023-02-08 20:28:34 -0600

    Update YearlyMaintenanceCost.java

--- src/YearlyMaintenanceCost.java
+++ src/YearlyMaintenanceCost.java
@@ -1,5 +1,16 @@
-public class Main {
+public class YearlyMaintenanceCost {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Initalize variables
+        int springCost = 400;
+        int summerCost = 900;
+        int fallCost = 600;
+        int winterCost = 200;
+
+        //Add the variables
+        int totalCost = springCost + summerCost + fallCost + winterCost;
+
+        //Output the values and the total
+        System.out.println("With the costs of maintenance for Spring($" + springCost + "), Summer($" + summerCost + "), Fall($" + fallCost + "), and Winter($" + winterCost + "), your total yearly maintenance cost is $" + totalCost + ".");
+
     }
 }
